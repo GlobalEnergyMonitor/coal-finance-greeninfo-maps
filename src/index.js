40f539ff6c3ae0a03616db46fdbb6a60d326e99f
Taylor Higgins <126080744+TaylorHigginsGEM@users.noreply.github.com>
2025-04-18 23:47:41 -0400

    Update index.js to go around no npm build
    rename new data file with old file name coal-finance-gi-map 2024-10-28

--- src/index.js
+++ src/index.js
@@ -90,7 +90,7 @@ CONFIG.first_load = true;
 $(document).ready(function () {
 
   // data initialization first, then the remaining init steps
-  Promise.all([initData('./data/gcpft_map_2025_april_18.csv'), initData('./data/countries.json')])
+  Promise.all([initData('./data/coal-finance-gi-map 2024-10-28.csv'), initData('./data/countries.json')])
     .then(function(data) {
       initDataFormat(data);    // get data ready for use
       initLeafletShims();      // Leaflet extensions and shims
